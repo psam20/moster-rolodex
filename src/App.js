ff84c02c2d7969a2673b3d62aba9a60562a6536a
Kishan Gupta <unique.kishan.gupta@gmail.com>
2019-12-23 02:11:00 +0530

    Setup project monster rolodex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  
+  constructor(){
+    // Super method calls the constructor method on the Component class 
+    super();
+    // now our App gets access to the state property after calling Super
+    this.state ={
+      string : "Hello kishan"
+    }
+      
+    
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+           <p>{this.state.string}</p>
+           <p>{this.state.string}</p>
 
+           <button onClick={() => {this.setState({string :" hello gupta ji "})}}>CLick me</button>
+        
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
